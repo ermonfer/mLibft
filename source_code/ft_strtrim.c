e00411df527e60ed6fe26bb0ddc40f10ffba19aa
fer <fer>
2024-04-09 12:05:04 +0200

    casa

--- source_code/ft_strtrim.c
+++ source_code/ft_strtrim.c
@@ -6,11 +6,11 @@
 /*   By: fmontero <fmontero@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 18:42:39 by fmontero          #+#    #+#             */
-/*   Updated: 2024/04/08 21:43:30 by fmontero         ###   ########.fr       */
+/*   Updated: 2024/04/08 22:39:00 by fmontero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.c"
+#include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -19,11 +19,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (ft_isinchr(*s1, set))
 		s1++;
 	if (s1 == '\0')
-		return ()
-	while (*s1 == '\0')
-		s1++;
-	
-
-	
-
-
+		return (strdup(s1));
+	begin = s;
+	while (*++s1 != '\0')
+		;
+	while (ft_isinchr(*--s1, set))
+		;
+	return (ft_substr(begin, 0, end - begin + 1));
+}
