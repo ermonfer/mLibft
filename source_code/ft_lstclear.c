a2c88aef4e05e906f675ced573fecf7616bdfdc2
Fernando Montero Erustes <fmontero@c3r10s3.42madrid.com>
2024-04-15 14:39:04 +0200

    arreglando

--- source_code/ft_lstclear.c
+++ source_code/ft_lstclear.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+<<<<<<< HEAD
+/*   By: fmontero <fmontero@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 12:25:27 by fmontero          #+#    #+#             */
+/*   Updated: 2024/04/15 14:17:24 by fmontero         ###   ########.fr       */
+=======
 /*   By: fmontero <fmontero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 00:18:00 by fmontero          #+#    #+#             */
 /*   Updated: 2024/04/12 00:21:43 by fmontero         ###   ########.fr       */
+>>>>>>> b785db0a2f3babd23aa98de839e6fac06ea89aa8
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+<<<<<<< HEAD
+#include <unistd.h>
+
+void	ft_lstclear(t_list **lst, void (*del)(*void))
+{
+	t_list		*node;
+
+	while (*lst != NULL)
+	{
+		node = *lst;
+		*lst = *lst -> next;
+		del(node -> content);
+		free(node);
+	}
+}
+=======
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
@@ -22,3 +45,4 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	*lst = NULL;
 }
 
+>>>>>>> b785db0a2f3babd23aa98de839e6fac06ea89aa8
--- source_code/ft_lstmap.c
+++ source_code/ft_lstmap.c
@@ -1,3 +1,39 @@
+<<<<<<< HEAD
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmontero <fmontero@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 14:05:19 by fmontero          #+#    #+#             */
+/*   Updated: 2024/04/15 14:07:37 by fmontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list		*node;
+	t_list		*new_list;
+
+	new_lst = NULL;
+	while (lst != NULL)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (node == NULL)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
+=======
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	
+>>>>>>> b785db0a2f3babd23aa98de839e6fac06ea89aa8
